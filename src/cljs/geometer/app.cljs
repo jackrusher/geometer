3fff9fac0e20aa6d829bc42c6713cce2a71b2055
Jack Rusher <jack@rusher.com>
2015-11-23 10:29:55 +0100

    Added some comments.

--- src/cljs/geometer/app.cljs
+++ src/cljs/geometer/app.cljs
@@ -11,14 +11,23 @@
             [thi.ng.geom.webgl.animator :refer [animate]]
             [thi.ng.geom.webgl.shaders.basic :as basic]))
 
+;; we use defonce for the webgl context and mouse tracking atoms so
+;; they won't be re-initialed when the namespace is reloaded
+(defonce gl (gl/gl-context "main"))
+(defonce view-rect (gl/get-viewport-rect gl))
 (defonce mouse-x (atom 0))
 (defonce mouse-y (atom 0))
 
-(defonce gl (gl/gl-context "main"))
-(defonce view-rect (gl/get-viewport-rect gl))
-
 (def model
+  "Our model is a collection of vertices and colors that will be rendered by the animation loop started in `start`."
+  ;; This vector of 1s and -1s is a pattern that represents the
+  ;; relative positions of a series of points that make a series of
+  ;; triangles that make a cube. We add some random jitter to those
+  ;; points to make the shape slightly more interesting. Removing the
+  ;; call to `(rand)` will cause the cube to coalesce.
   (let [vertices (map #(* % (+ 0.3 (* 0.3 (rand)))) [1 -1 1 1 -1 -1 1 1 -1 1 -1 1 1 1 -1 1 1 1 -1 -1 -1 -1 -1 1 -1 1 1 -1 -1 -1 -1 1 1 -1 1 -1 -1 1 -1 -1 1 1 1 1 1 -1 1 -1 1 1 1 1 1 -1 -1 -1 -1 1 -1 -1 1 -1 1 -1 -1 -1 1 -1 1 -1 -1 1 -1 -1 1 1 -1 1 1 1 1 -1 -1 1 1 1 1 -1 1 1 -1 -1 -1 -1 1 -1 1 1 -1 -1 -1 -1 1 1 -1 1 -1 -1])
+        ;; We color the six points for the six faces of the cube by
+        ;; repeating each of these six colors six times.
         colors   (flatten (map (partial repeat 6)
                                [[0.9 0.3 0.3 1]
                                 [0.3 0.9 0.3 1]
@@ -26,6 +35,8 @@
                                 [0.9 0.9 0.3 1]
                                 [0.9 0.3 0.9 1]
                                 [0.0 0.9 0.9 1]]))]
+    ;; ... and then pack it all into a data structure that geom's
+    ;; WebGL interface understands
     (-> {:attribs {:position {:data (arrays/float32 vertices) :size 3}
                    :color    {:data (arrays/float32 colors) :size 4}}
          :uniforms     {:proj (gl/perspective 45 view-rect 0.1 100.0)
@@ -36,7 +47,9 @@
                             (sh/make-shader-from-spec gl))}
                       (buf/make-attribute-buffers-in-spec gl gl/static-draw))))
 
-(defn ^:export start []
+(defn ^:export start
+  "This function is called when `index.html` is loaded. We use it to kick off the mouse tracking function and the animation loop."
+  []
   (.addEventListener
    js/document "mousemove"
    #(do
