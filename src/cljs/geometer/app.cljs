797d8545246739a0c820a90a8267a9ef136256cf
Karsten Schmidt <k@postspectacular.com>
2016-01-26 08:34:48 +0000

    refactor view-rect & adapt to window resizing, add resize handler update projection matrix, cache shader, simplify mesh drawing

--- src/cljs/geometer/app.cljs
+++ src/cljs/geometer/app.cljs
@@ -3,6 +3,7 @@
             [thi.ng.geom.core.vector         :as v :refer [vec2 vec3]]
             [thi.ng.geom.core.matrix         :as mat :refer [M44]]
             [thi.ng.geom.core.utils          :as gu]
+            [thi.ng.geom.rect                :refer [rect]]
             [thi.ng.geom.webgl.core          :as gl]
             [thi.ng.geom.webgl.animator      :refer [animate]]
             [thi.ng.geom.webgl.buffers       :as buf]
@@ -22,14 +23,17 @@
 ;; we use defonce for the webgl context, mouse tracking atoms and
 ;; model they won't be re-initialed when the namespace is reloaded
 (defonce gl (gl/gl-context "main"))
-(defonce view-rect (gl/get-viewport-rect gl))
+(defonce view-rect (atom (gl/get-viewport-rect gl)))
 
 (defonce mouse-x (atom 0))
 (defonce mouse-y (atom 0))
 
+(defonce projection (atom nil))
 (defonce model (atom nil))
 
 (defonce viewpoint (atom (g/translate M44 0 0 -70)))
+
+(defonce shader (sh/make-shader-from-spec gl phong/shader-spec))
 
 ;; (defn- vary-color
 ;;   "Add random variance to an (r,g,b,a) sequence of colors values."
@@ -49,7 +53,7 @@
 ;;                 gl (gl/gl-context "main")]
 ;;             (-> {:attribs {:position {:data (float32 vertices) :size 3}
 ;;                            :color    {:data (float32 colors) :size 4}}
-;;                  :uniforms     {:proj (gl/perspective 45 view-rect 0.1 1200.0)
+;;                  :uniforms     {:proj (gl/perspective 45 @view-rect 0.1 1200.0)
 ;;                                 :view (mat/look-at (v/vec3 0 0 0) (v/vec3) v/V3Y)}
 ;;                  :mode         gl/triangles
 ;;                  :num-vertices (/ (count vertices) 3)
@@ -64,15 +68,16 @@
           (-> (g/center mesh)
               (gl/as-webgl-buffer-spec {})
               (buf/make-attribute-buffers-in-spec gl gl/static-draw)
-              (assoc :shader (sh/make-shader-from-spec gl phong/shader-spec))
+              (assoc :shader shader)
               (update-in [:uniforms] merge
-                         {:proj          (gl/perspective 45 view-rect 0.1 100.0)
+                         {:proj          @projection
+                          :view          (mat/look-at (vec3 0 0 2) (vec3) (vec3 0 1 0))
                           :lightPos      (vec3 2 0 5)
                           :ambientCol    0x181818
                           :diffuseCol    0x10a04c
-                          :specularCol   0x888888
+                          :specularCol   0xaaaaaa
                           :shininess     100
-                          :wrap          5
+                          :wrap          1
                           :useBlinnPhong true}))))
 
 ;; (apply str (map #(format "%x" (int (* 255 %))) [0.064 0.63 0.30]))
@@ -104,6 +109,16 @@
       115 (reset! viewpoint (g/translate @viewpoint 0 0 -2)) ;; s = backward
       (print k))))
 
+(defn resize-handler
+  []
+  (let [w (.-innerWidth js/window)
+        h (.-innerHeight js/window)
+        c (.getElementById js/document "main")]
+    (set! (.-width c) w)
+    (set! (.-height c) h)
+    (reset! view-rect (rect 0 0 w h))
+    (reset! projection (gl/perspective 45 @view-rect 0.1 200.0))))
+
 (defn- update-pos [e]
   (reset! mouse-x (* 0.01 (- (.-clientX e) (/ (.-innerWidth js/window) 2))))
   (reset! mouse-y (* 0.01 (- (.-clientY e) (/ (.-innerHeight js/window) 2)))))
@@ -116,13 +131,15 @@
   (.addEventListener js/window "touchmove"
                      #(do (.preventDefault %)
                           (update-pos (aget (.-touches %) 0))))
-  
+  (.addEventListener js/window "resize" resize-handler)
+  (resize-handler)
+
   ;; initialize with a cube
   (set-model! (shapes/cube))
-  
+
   (animate
    (fn [[t frame]]
-     (gl/set-viewport gl view-rect)
+     (gl/set-viewport gl @view-rect)
      (gl/clear-color-buffer gl 0 0 0 0) ;; 0 opacity, so we see the bg gradient
      (gl/clear-depth-buffer gl 1)
      (gl/enable gl gl/depth-test)
@@ -130,14 +147,12 @@
      ;;  gl (-> model
      ;;         (assoc-in [:uniforms :model] tx2)
      ;;         (assoc-in [:uniforms :diffuseCol] 0x33ff80)))
-     (let [m (assoc-in
-              @model [:uniforms :view]
-              (mat/look-at (vec3 0 0 2) (vec3) (vec3 0 1 0)))]
-       (phong/draw gl (assoc-in m
-                                [:uniforms :model]
-                                (-> @viewpoint
-                                    (g/translate 0 0 0)
-                                    (g/rotate-x @mouse-y)
-                                    (g/rotate-y @mouse-x)))))
-     
+     (phong/draw
+      gl (update @model :uniforms merge
+                 {:proj  @projection
+                  :model (-> @viewpoint
+                             (g/translate 0 0 0)
+                             (g/rotate-x @mouse-y)
+                             (g/rotate-y @mouse-x))}))
+
      true)))
